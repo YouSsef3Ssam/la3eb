29e495d6bf53f1dea48e98ca2ee069339099e5db
Youssef Essam <youssef.3ssam1@gmail.com>
2022-07-22 07:05:14 +0200

    Make games request

--- app/src/main/java/com/youssef/task/framework/presentation/features/games/GamesFragment.kt
+++ app/src/main/java/com/youssef/task/framework/presentation/features/games/GamesFragment.kt
@@ -1,21 +1,35 @@
 package com.youssef.task.framework.presentation.features.games
 
+import androidx.fragment.app.viewModels
 import com.youssef.task.R
 import com.youssef.task.databinding.FragmentGamesBinding
 import com.youssef.task.framework.presentation.features.base.BaseFragment
 import com.youssef.task.framework.utils.ext.navigateTo
+import com.youssef.task.framework.utils.states.DataState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GamesFragment : BaseFragment<FragmentGamesBinding>() {
+    private val viewModel by viewModels<GamesViewModel>()
 
     override fun bindViews() {
         initUI()
+        subscribeOnViewObservers()
     }
 
     private fun initUI() {
         binding.text.setOnClickListener { navigateTo(GamesFragmentDirections.openGameDetails(1)) }
 
+    }
+
+    private fun subscribeOnViewObservers() {
+        viewModel.gamesDataState.observe(viewLifecycleOwner) {
+            when (it) {
+                is DataState.Success -> showMessage(it.data.count.toString())
+                is DataState.Failure -> handleError(it.throwable)
+                DataState.Loading -> showMessage("Loading")
+            }
+        }
     }
 
     override fun getLayoutResId() = R.layout.fragment_games
---
+++ app/src/main/java/com/youssef/task/framework/presentation/features/games/GamesViewModel.kt
@@ -0,0 +1,37 @@
+package com.youssef.task.framework.presentation.features.games
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.youssef.task.business.entities.Game
+import com.youssef.task.business.entities.Response
+import com.youssef.task.business.usecases.abstraction.GamesUseCase
+import com.youssef.task.framework.utils.states.DataState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GamesViewModel @Inject constructor(private val useCase: GamesUseCase) : ViewModel() {
+    private val _gamesDataState: MutableLiveData<DataState<Response<List<Game>>>> =
+        MutableLiveData()
+    val gamesDataState: LiveData<DataState<Response<List<Game>>>> get() = _gamesDataState
+
+    init {
+        getGames(1, 10)
+
+    }
+
+    private fun getGames(pageNumber: Int, pageSize: Int) {
+        viewModelScope.launch {
+            useCase.getGames(1, 10)
+                .onStart { _gamesDataState.value = DataState.Loading }
+                .catch { _gamesDataState.value = DataState.Failure(it) }
+                .collect { _gamesDataState.value = DataState.Success(it) }
+        }
+    }
+
+}
